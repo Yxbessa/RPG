b00035fa4b3c3cff74ed906d4462d32605ec5931
Yxbessa <yagobessa18@gmail.com>
2025-08-04 21:54:14 -0300

    .gitigore. e interface atualizada

--- Interface.py
+++ Interface.py
@@ -1,6 +1,10 @@
-name = input("Escolha seu nome:")
-print("Escolha sua classe entre:")
-print("1. Guerreiro")
-print("2. Mago")
-print("3. Arqueiro")
-character_class = input()
+from classes import Character
+def get_character_input():
+    name = input("Escolha seu nome:")
+    print("Escolha sua classe entre:")
+    print("1. Guerreiro")
+    print("2. Mago")
+    print("3. Arqueiro")
+    character_class = input()
+    return Character(name, character_class)
+character = get_character_input()
