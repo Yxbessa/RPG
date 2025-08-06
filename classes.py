8e8984b13bf6d526761c64bdf6d69358c33497de
Yxbessa <yagobessa18@gmail.com>
2025-08-05 23:31:28 -0300

    Classe criada, ainda n sei q tipo de RPG fazer

--- classes.py
+++ classes.py
@@ -1,8 +1,9 @@
 class Character:
-    def __init__(self, name, character_class, level=1):
+    def __init__(self, name, character_class):
         self.name = name
         self.character_class = character_class
-        self.level = level
+        self.level = 1
+        self.Xp = 0
 
     def level_up(self):
         self.level += 1
